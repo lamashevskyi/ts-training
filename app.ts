d86a0b557e6887f45687c21970d58283cd4a70c8
HustonBack <ivan.samiliak@gmail.com>
2019-04-18 01:27:03 +0300

    init

---
+++ app.ts
@@ -0,0 +1,60 @@
+// import * as _ from 'lodash'
+import { Claculator } from './oop'
+
+const calculator = new Claculator();
+calculator.add(5, 8);
+
+
+
+
+
+
+
+
+
+// const array = [1, 1, 2, 5];
+// const uniqArray = _.uniq(array);
+//
+// console.log(uniqArray);
+
+// let intVar;
+//
+// intVar = 1;
+// intVar = 'grjksgjh';
+// intVar = [];
+
+
+// const valid: boolean = true;
+//
+// const array: Array<0 | 1> = [0, 1, 1];
+// const uniqArray = _.uniq(array);
+//
+// console.log(uniqArray);
+//
+// function addAndPrint(a: number, b: number): void {
+//     console.log(a + b);
+// }
+//
+// addAndPrint(6, 6);
+
+// async function request (params: object): Promise<number> {
+//     return 5;
+// }
+
+// type Input = {
+//     id: number,
+//     name: string,
+//     status?: boolean
+// }
+//
+// type Output = {
+//     items: Array<string>,
+//     count: number
+// }
+//
+//
+// function parse (input: Input): Output {
+//     return { items: ['sfkufrl'], count: 1 }
+// }
+//
+// parse({id: 1, name: 'ghghf'});
